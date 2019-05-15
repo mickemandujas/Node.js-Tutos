7a69ac31188375c0afb20d82b3f16d7f49067bdb
Miguel Mandujano <mickemandu@outlook.com>
2019-05-15 01:17:22 -0500

    Lesson 4

---
+++ MitoCode/app.js
@@ -0,0 +1,15 @@
+const os = require('os');
+const fs = require('fs');
+let cpu = os.cpus();
+let sistema = os.platform();
+let usuario = os.hostname();
+
+/*console.log(cpu);
+console.log(sistema);
+console.log(usuario);*/
+let cpu_string = JSON.stringify(cpu);
+fs.appendFile('mickemandujas.txt', `Información del cpu: ${cpu_string} `, function(error){
+    if (error) {
+        console.log('Error al crear el archivo');
+    }
+});
