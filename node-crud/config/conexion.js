b846695feb9faff12a2823cd76a0cf6cc0d907e8
Miguel Mandujano <mickemandu@outlook.com>
2019-05-21 21:18:21 -0500

    mLab

--- node-crud/config/conexion.js
+++ node-crud/config/conexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/crud', { useMongoClient: true }); //mongodb://localhost:27017/crud
-mongoose.connect('mongodb://localhost/crud', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://mickemandujas:123@clusterfornodejs-2fzle.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 
 module.exports = mongoose;
