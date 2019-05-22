a0becd3300a4dc1e80f0b66548955a1c82c8f5e4
Miguel Mandujano <mickemandu@outlook.com>
2019-05-21 22:56:32 -0500

    Last commit to deploy in heroku and mLab or atlas mongodb

--- node-crud/config/conexion.js
+++ node-crud/config/conexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/crud', { useMongoClient: true }); //mongodb://localhost:27017/crud
-mongoose.connect('mongodb+srv://mickemandujas:123@clusterfornodejs-2fzle.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://mickemandujas:1234@clusterfornodejs-2fzle.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 
 module.exports = mongoose;
